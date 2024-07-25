ea1c5dd719182e2f55c50b2d41fc4dd11c5fe105
gabriel-camara-dev <galvares439@gmail.com>
2024-07-25 16:04:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+let input = document.querySelector('#textarea')
+let caixa = document.querySelector('#caixa')
+let historico_mensagens = document.querySelector('.historico_mensagens')
+
+let btn_enviar = document.querySelector('.enviar')
+btn_enviar.addEventListener('click', (evento) => {
+    evento.preventDefault()
+    
+    mensagem = input.value
+    input.value = ''
+    
+    let caixa_mensagem = document.createElement('div')
+    caixa_mensagem.classList.add('caixa_mensagem')
+    
+    let texto = document.createElement('p')
+    texto.classList.add('texto')
+    caixa_mensagem.append(texto)
+    texto.textContent = mensagem
+    
+    let editar_excluir = document.createElement('div')
+    editar_excluir.classList.add('editar_excluir')
+    
+    let botao_excluir = document.createElement('p')
+    botao_excluir.classList.add('botao_excluir')
+    botao_excluir.textContent = 'Excluir'
+    editar_excluir.append(botao_excluir)
+    
+    botao_excluir.onclick = () => {
+        editar_excluir.remove()
+        caixa_mensagem.remove()
+    }
+    
+    let botao_editar = document.createElement('p')
+    botao_editar.classList.add('botao_editar')
+    botao_editar.textContent = 'Editar'
+    editar_excluir.append(botao_editar)
+
+    botao_editar.onclick = () => {
+        const mensagemEditada = prompt('Qual a nova mensagem?')
+        texto.textContent = mensagemEditada
+    }
+
+    historico_mensagens.append(caixa_mensagem)
+    historico_mensagens.append(editar_excluir)
+})
+
+
+
+
